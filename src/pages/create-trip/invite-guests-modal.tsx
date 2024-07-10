767f8896dfbdd1d1390d8bc810c0065155bcae44
limaofelipe <dev.felipelima@gmail.com>
2024-07-10 17:59:36 -0300

    creating components

---
+++ src/pages/create-trip/invite-guests-modal.tsx
@@ -0,0 +1,66 @@
+import { AtSign, Plus, X } from "lucide-react"
+import { FormEvent } from "react";
+
+interface InvitGuestsModalProps {
+    closeGuestsModal: () => void
+    emailsToInvite: string[]
+    addNewEmailToInvite: (event: FormEvent<HTMLFormElement>) => void
+    removeEmailFromInvites: (email: string) => void
+
+}
+
+export function InviteGuestsModal({ addNewEmailToInvite, closeGuestsModal, emailsToInvite, removeEmailFromInvites  }: InvitGuestsModalProps) {
+
+
+    return (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+            <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+                <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                    <h2 className="text-lg font-semibold">Selecionar Convidado</h2>
+                    <button onClick={closeGuestsModal} type="button">
+                    <X className="size-5 text-zinc-400"/>
+                    </button>
+                </div>
+                <p className="text-sm text-zinc-400 text-left">
+                    Os convidados irão receber e-mails para confirmar a participação na viagem.
+                </p>
+                </div>
+
+                <div className="flex flex-wrap gap-2">
+                {emailsToInvite.map(email =>{
+                    return (
+                    <div key={email} className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-8 ">
+                    <span className="text-zinc-300">{email}</span>
+                    <button type="button">
+                        <X onClick={() => removeEmailFromInvites(email)} className="size-4 text-zinc-400" />
+                    </button>
+                    </div>
+                    )
+                })}
+                </div>
+
+                <div className="w-full h-px bg-zinc-800"/>
+
+                <form onSubmit={addNewEmailToInvite} className="flex items-center gap-2 p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg">
+                <div className="px-2 flex items-center flex-1 gap-2">
+                    <AtSign className="text-zinc-400 size-5"/>
+
+                    <input className="outline-none bg-transparent text-lg
+                    placeholder-zinc-400 flex-1" 
+                    type="email" 
+                    name="email"
+                    placeholder="Digite o email do convidado"
+                    />
+                </div>
+
+                <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                    <Plus className="size-5"/>
+                    Convidar
+                </button>
+
+                </form>
+            </div>
+            </div>
+        )
+}
---
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -0,0 +1,46 @@
+import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+
+interface DestinationAndDateStepProps {
+    isGuestsInputOpen: boolean
+    closeGuestsInput: () => void
+    openGuestsInput: () => void
+}
+
+export function DestinationAndDateStep({closeGuestsInput, openGuestsInput, isGuestsInputOpen} : DestinationAndDateStepProps) {
+    return (
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+        <div className="flex items-center gap-2 flex-1">
+          <MapPin className="size-5 text-zinc-400"/>
+          <input disabled={isGuestsInputOpen} className="outline-none bg-transparent text-lg
+            placeholder-zinc-400 flex-1" 
+            type="text" placeholder="Para onde você vai?"
+          />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Calendar className="size-5 text-zinc-400"/>
+          <input disabled={isGuestsInputOpen} className="outline-none bg-transparent text-lg
+            placeholder-zinc-400 w-40"
+            type="text" placeholder="Quando"
+          />
+        </div>
+
+
+        <div className="w-px h-6 bg-zinc-800"/>
+
+
+      {isGuestsInputOpen ? (
+        <button onClick={closeGuestsInput} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap 2 hover:bg-zinc-700"> 
+          Alterar Local e data
+          <Settings2 className="size-5"/>
+        </button>
+      ) : (
+            <button onClick={openGuestsInput} className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap 2 hover:bg-lime-400">
+            <ArrowRight className="size-5"/>
+            Continuar
+            </button>
+      )}
+
+      </div>
+    )
+}
